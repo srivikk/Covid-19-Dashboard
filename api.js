590e2069073a5ddcc2eeef0e6eecea0630b4c5df
Vikrant <srivasvik@gmail.com>
2020-06-01 17:49:15 +0530

    data added as json

--- api.js
+++ api.js
@@ -1,90 +1,102 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const url = "mongodb://localhost:27017/source_scrapped_data";
 const app = express();
-mongoose.connect(url, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log("Connected to mongo");
-});
+const fs = require('fs');
+// const Schema = mongoose.Schema;
+// const url = "mongodb://localhost:27017/source_scrapped_data";
 
-const excludedList = ["NorthAmerica", "SouthAmerica", "Europe", "","Asia","Africa","Oceania","World"]
-const covid_data = mongoose.model('Covid',
-    new Schema({}),
-    'covid_data');
+// mongoose.connect(url, { useNewUrlParser: true });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', () => {
+//     console.log("Connected to mongo");
+// });
+
+
+const docs = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+// const excludedList = ["NorthAmerica", "SouthAmerica", "Europe", "", "Asia", "Africa", "Oceania", "World"]
+// const covid_data = mongoose.model('Covid',
+//     new Schema({}),
+//     'covid_data');
 
 app.get("/data", (req, res) => {
-    covid_data.find().exec(function (err, docs) {
-        let transformedDocs = docs.map(function (doc) {
-            return doc.toJSON();
-        });
-        if (err) {
-            console.log(err);
-            return res.status(500).send(err);
+    // covid_data.find().exec(function (err, docs) {
+    //     let transformedDocs = docs.map(function (doc) {
+    //         return doc.toJSON();
+    //     });
+    //     if (err) {
+    //         console.log(err);
+    //         return res.status(500).send(err);
+    //     }
+    //     else {
+    let transformedDocs = docs
+    transformedDocs.forEach(function (doc, index) {
+        if (excludedList.includes(doc['Country,Other'])) {
+            delete transformedDocs[index]
         }
-        else {
-
-            transformedDocs.forEach(function (doc, index) {
-                if (excludedList.includes(doc['Country,Other'])) {
-                    delete transformedDocs[index]
-                }
-            })
-            transformedDocs = transformedDocs.filter(function (el) {
-                return el != null;
-              })
-            res.send(transformedDocs);
-        }
-    });
+    })
+    transformedDocs = transformedDocs.filter(function (el) {
+        return el != null;
+    })
+    res.send(transformedDocs);
+    //     }
+    // });
 });
 
 app.get("/aggregateData", (req, res) => {
-    covid_data.find({ "Country,Other": "World" }, (err, docs) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).send(err);
+    // covid_data.find({ "Country,Other": "World" }, (err, docs) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return res.status(500).send(err);
+    //     }
+    //     else {
+    //         res.send(docs);
+    //     }
+    // });
+    docs.forEach(function (doc) {
+        if (doc['Country,Other'] === "World") {
+            res.send(doc)
         }
-        else {
-            res.send(docs);
-        }
-    });
+    })
 });
 
 app.get("/aggregateDatadisposable", (req, res) => {
-    const continent = req.query.continent;
-    covid_data.aggregate(
-        [
+    // const continent = req.query.continent;
+    // covid_data.aggregate(
+    //     [
 
-            {
-                $match: { "Continent": continent }
-            }
-        ], (err, docs) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).send(err);
-            }
-            else {
-                res.send(docs[0]);
-            }
-        }
-    )
+    //         {
+    //             $match: { "Continent": continent }
+    //         }
+    //     ], (err, docs) => {
+    //         if (err) {
+    //             console.log(err);
+    //             return res.status(500).send(err);
+    //         }
+    //         else {
+    //             res.send(docs[0]);
+    //         }
+    //     }
+    // )
+    res.send(docs[0])
 });
 
 
 app.get("/contdata", (req, res) => {
-    console.log(req.query);
+    // console.log(req.query);
 
-    const continent = req.query.continent;
-    covid_data.find({ "Continent": continent }, (err, docs) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).send(err);
-        }
-        else {
-            res.send(docs);
-        }
-    });
+    // const continent = req.query.continent;
+    // covid_data.find({ "Continent": continent }, (err, docs) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return res.status(500).send(err);
+    //     }
+    //     else {
+    //         res.send(docs);
+    //     }
+    // });
+    res.send(docs)
 });
 
 module.exports = app;
